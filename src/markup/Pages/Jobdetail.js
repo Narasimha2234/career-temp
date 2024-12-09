2bb5bbe66a348221855809c739346bfd0927e6a1
narasimha-orchasp <t-narasimhaswamy@orchasp.com>
2024-12-09 14:49:38 +0530

    final

--- src/markup/Pages/Jobdetail.js
+++ src/markup/Pages/Jobdetail.js
@@ -102,7 +102,7 @@ function Jobdetail(){
 										
 										<h5 className="font-weight-600">How to Apply</h5>
 										<div className="dez-divider divider-2px bg-gray-dark mb-4 mt-0"></div>
-										<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.</p>
+										<p>Create an account by completing the registration process. Once registered, log in to your account using your credentials.Update your candidate profile by filling in all required fields, including personal details, educational qualifications, skills, and uploading your resume. A complete profile ensures you can apply for jobs seamlessly.Make sure your profile is up-to-date to increase your chances of getting shortlisted.Browse through the available job listings and click on the Apply button for the roles that match your skills and qualifications.</p>
 										
 										<h5 className="font-weight-600">Job Requirements</h5>
 										<div className="dez-divider divider-2px bg-gray-dark mb-4 mt-0"></div>
